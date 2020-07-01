b1eb2b3bd58196ba0ef8f1954180cbbb0b6e91c4
Thomas Cooper <thomascooper@Thomass-MBP.fios-router.home>
2020-06-30 21:33:22 -0400

    Made Oggi's edits

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-$(document).ready(function (){
-	$.ajax({
-		url: "http://restcountries.eu/rest/v2/name/estonia",
-		success: function(response) {
-			console.log(response);
-		}
-	});
-});
+// $(document).ready(function (){
 
+// let input = document.getElementById("input");
+// let button = document.getElementById("searchButton");
+
+// button.addEventListener("click", function () {
+// $.ajax({
+// url: "https://restcountries.eu/rest/v2/name/" + input.value,
+// success: function(response) {
+// console.log(response);
+// }
+// });
+// });
+// let wrapper = document.createElement("DIV");
+// wrapper.innerHTML = input.value;
+// wrapper.innerHTML += "<br>Capital: " + ;
+// document.getElementById("content").appendChild(wrapper);
+// });
+
+
+$(document).ready(function (){
+
+let submit = document.getElementById("submit-country");
+let inputCountry = document.getElementById("country");
+
+submit.addEventListener("click", countryAjaxCall)
+
+function countryAjaxCall () {
+	$.ajax({
+		url: "https://restcountries.eu/rest/v2/name/" + inputCountry.value,
+		success: function(response) {
+			displayCountryData(response);
+		}
+	})
+}
+
+function displayCountryData(countries) {
+	$("#country-name").html("Name of the country: " + countries[0].name);
+}
+
+
+});
