c19838fe4803c2f8a2c6bbdd7b68d2f8acc77aa5
Thomas Cooper <thomascooper@Thomass-MBP.fios-router.home>
2020-07-07 21:06:05 -0400

    Created one language button and formatted population number

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function countryAjaxCall () {
 		url: "https://restcountries.eu/rest/v2/name/" + inputCountry.value,
 		success: function(response) {
 			displayCountryData(response);
+			translateFrench(response);
 			// console.log(response)
 		}
 	})
@@ -22,7 +23,19 @@ function displayCountryData(countries) {
 		if (countries[i].name.toLowerCase() == inputCountry.value.toLowerCase()) {
 			$("#country-name").html("Name of the country: " + countries[0].name);
 			$("#capital").html("Capital: " + countries[i].capital);
-			$("#population").html("Population: " + countries[i].population);
+	
+			let currentPopulation = countries[i].population;
+
+			function formatNumber(num) {
+				var str = num.toString().split('.');
+    			if (str[0].length >= 5) {
+       				str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    			}
+    			$("#population").html("Population: " + str.join('.'));
+			}
+
+			formatNumber (currentPopulation);
+
 			$("#timezones").html("Timezones: " + countries[i].timezones);
 			$("#flag").html("<img class='flagImg' src='" + countries[i].flag + "'/>");
 			$(".flagImg").css({
@@ -31,9 +44,14 @@ function displayCountryData(countries) {
 			});
 		}
 	}
+}
 
-	
+function translateFrench(countries) {
+	let frenchButton = document.getElementById("French");
 
+	frenchButton.addEventListener("click", function () {
+		console.log(countries[0].translations.fr);
+	});
 }
 
 
